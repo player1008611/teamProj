b957b8e5d3c341d31b7a9aef79474083fe2a2ac3
xyx <1046327131@qq.com>
2023-10-26 17:10:28 +0800

    v1.0

---
+++ src/main/java/com/teamproj/TeamProjApplication.java
@@ -0,0 +1,13 @@
+package com.teamproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamproj/TeamProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
