e3423d179bb93390a2a89c22241c93c8b1f074cb
仿生代码蟹 <1046327131@qq.com>
2023-11-28 12:55:37 +0800

    v1.6.20.1(更改token有效期为一天)

--- src/main/java/com/teamProj/utils/JwtUtil.java
+++ src/main/java/com/teamProj/utils/JwtUtil.java
@@ -28,22 +28,19 @@ public class JwtUtil {
      * 生成jwt
      *
      * @param subject token中要存放的数据（json格式）
-     * @return
      */
     public static String createJWT(String subject) {
         JwtBuilder builder = getJwtBuilder(subject, null, getUUID());// 设置过期时间
         return builder.compact();
     }
 
-    /**
-     * 生成jwt
-     *
-     * @param subject   token中要存放的数据（json格式）
-     * @param ttlMillis token超时时间
-     * @return
-     */
     public static String createJWT(String subject, Long ttlMillis) {
         JwtBuilder builder = getJwtBuilder(subject, ttlMillis, getUUID());// 设置过期时间
+        return builder.compact();
+    }
+
+    public static String createJWT(String id, String subject, Long ttlMillis) {
+        JwtBuilder builder = getJwtBuilder(subject, ttlMillis, id);// 设置过期时间
         return builder.compact();
     }
 
@@ -67,19 +64,6 @@ public class JwtUtil {
                 .setExpiration(expDate);
     }
 
-    /**
-     * 创建token
-     *
-     * @param id
-     * @param subject
-     * @param ttlMillis
-     * @return
-     */
-    public static String createJWT(String id, String subject, Long ttlMillis) {
-        JwtBuilder builder = getJwtBuilder(subject, ttlMillis, id);// 设置过期时间
-        return builder.compact();
-    }
-
     public static void main(String[] args) throws Exception {
         String token = createJWT("123456");
         Claims claims = parseJWT(token);
@@ -87,11 +71,6 @@ public class JwtUtil {
         System.out.println(claims);
     }
 
-    /**
-     * 生成加密后的秘钥 secretKey
-     *
-     * @return
-     */
     public static SecretKey generalKey() {
         byte[] encodedKey = Base64.getDecoder().decode(JwtUtil.JWT_KEY);
         SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length,
@@ -99,13 +78,6 @@ public class JwtUtil {
         return key;
     }
 
-    /**
-     * 解析
-     *
-     * @param jwt
-     * @return
-     * @throws Exception
-     */
     public static Claims parseJWT(String jwt) throws Exception {
         SecretKey secretKey = generalKey();
         return Jwts.parser()
