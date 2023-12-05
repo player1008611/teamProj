38a753749774f57221dcd7ee24f41a1872aa6713
player10086 <1825906144@qq.com>
2023-12-05 10:10:20 +0800

    1.7.1(实现学校端学生管理) 补充了缺失的注解

--- src/main/java/com/teamProj/dao/StudentDao.java
+++ src/main/java/com/teamProj/dao/StudentDao.java
@@ -1,10 +1,19 @@
 package com.teamProj.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.teamProj.entity.Student;
+import com.teamProj.entity.vo.AdminStudentVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 
 @Mapper
 public interface StudentDao extends BaseMapper<Student> {
+    IPage<AdminStudentVo> queryStudent(Page<AdminStudentVo> page
+            , @Param("name") String name
+            , @Param("schoolName") String schoolName
+            , @Param("status") Character status);
+
 }
